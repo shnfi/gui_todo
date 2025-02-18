842ee47e8debb89f816b1000e8bdbcbdc9b8b205
shnfi <mrplom7@gmail.com>
2025-02-18 21:03:15 +0330

    (fundumental structure conversion) moved the stylesheet code to the 'resources' directory

---
+++ resources/ss.cpp
@@ -0,0 +1,102 @@
+const QString main_SS = "\
+    background-color: #222222;\
+";
+
+const QString bar_SS = "\
+    #bar {\
+        border: 1px solid #404040; \
+        border-radius: 10px; \
+    }\
+";
+
+const QString todo_button_SS = "\
+    QPushButton {\
+        height: 27px; \
+        width: 82px; \
+        background-color: #202020;\
+        border: 1px solid #404040; \
+        color: #FAF9F6; \
+        border-radius: 5px; \
+    }\
+    QPushButton::hover {\
+        border: 1px solid #3498db; \
+    }\
+";
+
+const QString todo_field_SS = "\
+    QLineEdit { \
+        height: 27px; \
+        padding: 0 5px; \
+        background-color: #202020; \
+        border: 1px solid #404040; \
+        color: #FAF9F6; \
+        border-radius: 5px; \
+    }\
+    QLineEdit::focus {\
+        border: 1px solid #3498db; \
+    }\
+";
+
+const QString control_music_button_SS = "\
+    QPushButton {\
+        height: 27px; \
+        width: 27px; \
+        background-color: #202020;\
+        border: 1px solid #404040; \
+        color: #FAF9F6; \
+        border-radius: 10px; \
+    }\
+    QPushButton::hover {\
+        border: 1px solid #3498db; \
+    }\
+";
+
+const QString disabled_control_music_button_SS = "\
+    QPushButton {\
+        height: 27px; \
+        width: 27px; \
+        background-color:rgba(32, 32, 32, 0.45);\
+        border: 1px solid rgba(64, 64, 64, 0.45); \
+        color:rgba(250, 249, 246, 0.45); \
+        border-radius: 10px; \
+    }\
+";
+
+const QString todo_container_SS = "\
+    #todo_container {\
+        border: 1px solid #404040; \
+        border-radius: 5px; \
+    }\
+    #todo_container::hover {\
+        border: 1px solid #3498db; \
+    }\
+";
+
+const QString removing_todo_container_SS = "\
+    #todo_container {\
+        border: 1px solid #db3434; \
+        border-radius: 5px; \
+    }\
+";
+
+const QString remove_todo_button_SS = "\
+    QPushButton {\
+        height: 27px; \
+        width: 27px; \
+        background-color: #202020;\
+        border: 1px solid #404040; \
+        color: #FAF9F6; \
+        border-radius: 10px; \
+    }\
+    QPushButton::hover {\
+        border: 1px solid #db3434; \
+    }\
+";
+
+const QString music_name_label_SS = "\
+    color: #FAF9F6; \
+";
+
+const QString todo_label_SS = "\
+    color: #FAF9F6; \
+";
